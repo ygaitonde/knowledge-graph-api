9c4b0130ffd9fa246544739781715b9406bd62f4
yash.gaitonde@moderntreasury.com <gaitonde@umich.edu>
2022-08-04 14:34:35 -0700

    dont load json schema during rake tasks

--- config/initializers/load_schema.rb
+++ config/initializers/load_schema.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.after_initialize do
-  SchemaHelper.load_schema
+  SchemaHelper.load_schema unless File.basename($PROGRAM_NAME) == 'rake' 
 end
